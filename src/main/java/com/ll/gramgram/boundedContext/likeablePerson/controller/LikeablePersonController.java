efed67d0ef8f6e8aaf9ee8476dad042b252a20c1
Hypersand <on8214@gmail.com>
2023-04-06 15:28:23 +0900

    refactor: 불필요한 처리 삭제

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
@@ -67,10 +67,6 @@ public class LikeablePersonController {
 
         RsData<LikeablePerson> deleteRsData = likeablePersonService.deleteLikeablePerson(instaMember, id);
 
-        if (deleteRsData.isFail()) {
-            return rq.historyBack(deleteRsData);
-        }
-
         return rq.redirectWithMsg("/likeablePerson/list", deleteRsData);
     }
 }
